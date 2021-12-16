397bf28d63c014b4964af0fa5eca472c42bfa4d9
Akshay Sharma <akshaysharma@Akshays-MacBook-Pro.local>
2021-12-16 12:22:06 +0530

    change in helper

--- helpers/nodemailer.js
+++ helpers/nodemailer.js
@@ -2,10 +2,12 @@ const nodemailer = require("nodemailer");
 const sgMail = require("@sendgrid/mail");
 console.log(process.env.API_KEY);
 sgMail.setApiKey(
-  "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"
+  "SG.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"
 );
 const sendmail = async (req, res, userMail, subject, text) => {
   try {
+    console.log(subject, "subject"), console.log(text);
+    console.log(userMail, "maill");
     const msg = {
       to: userMail,
       from: "mastrolinks@gmail.com", // Use the email address or domain you verified above
